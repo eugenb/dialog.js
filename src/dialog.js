e9f31ff8db23f487957a3fc79fce128abed43de5
Eugen <dev@eugen.pro>
2020-01-05 01:05:56 +0200

    Cleanup, removed `srcElement` property (deprecated) and corrected how `nodeType` is retrieved

--- src/dialog.js
+++ src/dialog.js
@@ -20,16 +20,13 @@ class Dialog {
     constructor(body, args) {
 
         // Default options
-        //
         this.options = {
 
             // Classes
-            //
             dialogClassName: null,
             dialogPlaceholderClassName: null,
 
             // Sizes
-            //
             size: {
                 x: 0,
                 y: 0
@@ -37,17 +34,14 @@ class Dialog {
             position: {},
 
             // AutoShow
-            //
             autoShow: true,
 
             // Events
-            //
             autoClose: false,
             closeOnEsc: true,
             closeOnOutsideClick: true,
 
             // Callbacks
-            //
             callback: {
                 onBeforeShow: null,
                 onShow: null,
@@ -55,17 +49,14 @@ class Dialog {
             },
 
             // Link dialog relative to element
-            //
             linkTo: null
 
         };
 
         // Extend options
-        //
         this.options = Object.assign(this.options, args);
 
         // Create dialog
-        //
         this.create(body);
     }
 
@@ -79,7 +70,6 @@ class Dialog {
     static isChild(elem, dialog) {
 
         // Get descendents
-        //
         let d = dialog.getElementsByTagName('*');
         for (let i = 0; i < d.length; i++) {
             if (d[i] === elem) {
@@ -95,7 +85,6 @@ class Dialog {
     static closeAll() {
 
         // Close all open dialogs
-        //
         document.querySelectorAll('[dialog-id]').forEach(dlg => {
             if (typeof dlg.close === 'function') {
                 dlg.close();
@@ -111,17 +100,14 @@ class Dialog {
     create(body) {
 
         // Elements
-        //
         this.dlg = document.createElement('div');
         this.dlgPlaceholder = document.createElement('div');
 
         // Apply default classes
-        //
         this.dlgPlaceholder.classList.add('dialog-placeholder');
         this.dlg.classList.add('dialog');
 
         // Apply given classes
-        //
         if (this.options.dialogPlaceholderClassName !== null) {
             this.dlgPlaceholder.classList.add(this.options.dialogPlaceholderClassName);
         }
@@ -131,77 +117,62 @@ class Dialog {
         }
 
         // Set dialog placeholder attributes
-        //
         this.dlgPlaceholder.setAttribute('dialog-id', Math.random().toString(36).substr(2, 9));
         this.dlgPlaceholder.style.display = 'none';
 
         // Set dialog attributes
-        //
         this.dlg.setAttribute('dialog-id', Math.random().toString(36).substr(2, 9));
 
         // Set dialog body
-        //
         this.dlg.innerHTML = body;
 
         // Append dialog
-        //
         document.body.appendChild(this.dlgPlaceholder);
 
         // Calculate sizes
-        //
         this.options.size = {
             x: this.dlg.offsetWidth,
             y: this.dlg.offsetHeight
         };
 
         // Calculate viewport size(s)
-        //
         let viewportWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || 0,
             viewportHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
 
         // Render dialog linked to an existing element
-        //
         if (this.options.linkTo !== null) {
 
             // Move dialog next to linkTo element
-            //
             this.options.linkTo.parentNode.insertBefore(this.dlg, this.options.linkTo.nextSibling);
 
             // Set position coordinates based on linked element coords
-            //
             this.dlg.style.marginLeft = this.options.position.x !== undefined ? `${this.options.position.x}px` : 0;
             this.dlg.style.marginTop = this.options.position.y !== undefined ? `${this.options.position.y}px` : 0;
-        }
-
-        else {
+        } else {
 
             // Append dialog to placeholder
-            //
             this.dlgPlaceholder.appendChild(this.dlg);
 
             // Set position coordinates based on provided values
-            //
             this.dlg.style.marginLeft = this.options.position.x !== undefined ? `${this.options.position.x}px` : `${parseInt((viewportWidth - this.options.size.x) / 2)}px`;
             this.dlg.style.marginTop = this.options.position.y !== undefined ? `${this.options.position.y}px` : `${parseInt((viewportHeight - this.options.size.y) / 2)}px`;
         }
 
         // AutoClose
-        //
         if (this.options.autoClose) {
             setTimeout(() => {
                 this.close()
-            }, parseInt(this.options.autoClose * 1000));
+            }, parseInt(this.options.autoClose) * 1000);
         }
 
         // Close dialog on escape
-        //
         if (this.options.closeOnEsc) {
             document.addEventListener('keyup', e => {
 
                 let key = e.keyCode || e.which,
-                    target = e.target || e.srcElement;
+                    target = e.target;
 
-                if (target.parentType === 3) {
+                if (target.nodeType === 3) {
                     target = target.parentNode;
                 }
 
@@ -216,11 +187,10 @@ class Dialog {
         }
 
         // Close dialog when outside click
-        //
         if (this.options.closeOnOutsideClick) {
             this.dlgPlaceholder.addEventListener('click', e => {
 
-                let target = e.target || e.srcElement;
+                let target = e.target;
 
                 if (this.isVisible() && target !== this.dlg && !Dialog.isChild(target, this.dlg)) {
                     this.close();
@@ -229,22 +199,18 @@ class Dialog {
         }
 
         // Attach callbacks
-        //
         Object.defineProperty(this.dlg, 'show', {
             value: () => {
 
                 // Trigger onBeforeShow callback
-                //
                 if (typeof this.options.callback.onBeforeShow === 'function') {
                     this.options.callback.onBeforeShow();
                 }
 
                 // Show dialog
-                //
                 this.dlgPlaceholder.style.display = '';
 
                 // Trigger onBeforeShow callback
-                //
                 if (typeof this.options.callback.onShow === 'function') {
                     this.options.callback.onShow();
                 }
@@ -256,21 +222,17 @@ class Dialog {
             value: () => {
 
                 // Remove dialog
-                //
                 if (this.isVisible()) {
 
                     // Trigger onClose callback
-                    //
                     if (typeof this.options.callback.onClose === 'function') {
                         this.options.callback.onClose();
                     }
 
                     // Remove dialog
-                    //
                     this.dlg.parentNode.removeChild(this.dlg);
 
                     // Remove dialog placeholder
-                    //
                     this.dlgPlaceholder.parentNode.removeChild(this.dlgPlaceholder);
                     this.dlgPlaceholder = null;
                 }
@@ -279,7 +241,6 @@ class Dialog {
         });
 
         // Show dialog (if autoShow is true)
-        //
         if (this.options.autoShow) {
             this.show();
         }
